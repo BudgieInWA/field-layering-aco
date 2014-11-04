e84bdacb4b4057af6cf42a51814f9159c1407104
BudgieInWA <benjaminaritter@gmail.com>
2014-11-04 11:48:54 +0800

    Moved deterministic into aco.js and added a log file param

--- aco.js
+++ aco.js
@@ -758,6 +758,144 @@ ACO.prototype.runGeneration = function() {
 	this.solutions.generation_goodnesses.push(this.solutions.generation_best.goodness);
 }
 
+/*****************************************************************************
+ * Deterministic Algo                                                        *
+ *****************************************************************************/
+
+/// Calculates optimal linking for the most area covered.
+///
+/// This implementation uses a dynamic programming approach.
+/// time complexity: O(|thePortals| ^ 4)
+/// space complexity: O(|thePortals| ^ 3)
+function computeOptimalSingleTriangleLayering(thePortals) {
+  console.log("Computing Single Triangle Layering with", thePortals.length, "portals.");
+  console.log("^4 is", thePortals.length * thePortals.length * thePortals.length * thePortals.length);
+
+  var bestAns, bestAnsPortals;
+
+  // First we set up our cache for memoisation.
+  var cache = {};
+  function cache_get(ps) {
+    ps.sort();
+    if (!(ps[0] in cache)) return undefined;
+    if (!(ps[1] in cache[ps[0]])) return undefined;
+    if (!(ps[2] in cache[ps[0]][ps[1]])) return undefined;
+    return cache[ps[0]][ps[1]][ps[2]];
+  }
+  function cache_put(ps, value) {
+    ps.sort();
+    if (!(ps[0] in cache)) cache[ps[0]] = {};
+    if (!(ps[1] in cache[ps[0]])) cache[ps[0]][ps[1]] = {};
+    cache[ps[0]][ps[1]][ps[2]] = value;
+  }
+
+  // This is the recursive function, that calculates how many layers can be achieved when starting
+  // with a field made of the three portals in the list ps.
+  function howManyLayers(ps) {
+    var c = cache_get(ps);
+    if (c !== undefined) return c;
+
+    // We will try all possible larger fields that can be made by linking one extra portal to two
+    // of the portals in the current field, such that the new field covers the existing one.
+    // We will test each of these by recursing and we will choose the best one as our answer.
+    var bestVal = 0;
+    var bestParentPortals = undefined;
+    var bestReplacementPortal = undefined;
+    var bestAnchors = undefined;
+
+    // Here we test each other portal, recusing if it's suitable.
+    portalLoop:
+    for (var i = 0; i < thePortals.length; i++) {
+      var p = thePortals[i];
+
+      // The portal can't be one of the current portals.
+      if (i == ps[0] || i == ps[1] || i == ps[2]) continue;
+
+      // The portal can be on the "inside" (the same side as the field) of exactly one edge.
+      // If that is the case, the one such edge will be the base of the next layer.
+      var anchorA = undefined;
+      var anchorB = undefined;
+      var perms = [[ps[0], ps[1], ps[2]],  // The three edges we need to test.
+                   [ps[1], ps[2], ps[0]],
+                   [ps[2], ps[0], ps[1]]];
+      for (var j = 0; j < perms.length; j++) {
+        var perm = perms[j];
+        // Find out if the portal is on the "inside".
+        var side1 = side(thePortals[perm[0]], thePortals[perm[1]], thePortals[perm[2]]); 
+        var side2 = side(thePortals[perm[0]], thePortals[perm[1]], p);
+        if (side2 == 0) continue portalLoop; // Colinear portals should not be considered.
+        if (side1 == side2) {
+          if (anchorA === undefined) { // First "inside" portal.
+            anchorA = perm[0];
+            anchorB = perm[1];
+          } else { // Second "inside" portal (i.e. it's invalid)
+            continue portalLoop;
+          }
+        }
+      }
+
+      // p is valid so we can enclose the third portal by linking p to anchorA and anchorB.
+      var ans = howManyLayers([anchorA, anchorB, i]);
+      if (ans.value > bestVal) {
+        bestVal = ans.value;
+        bestParentPortals = [anchorA, anchorB, i];
+        bestReplacementPortal = p;
+        bestAnchors = [anchorA, anchorB];
+      }
+    }
+
+    // Add 1 for this field.
+    var ans = {
+      value: bestVal + triangleArea(thePortals[ps[0]], thePortals[ps[1]], thePortals[ps[2]]),
+      parentPortals: bestParentPortals,
+      replacementPortal: bestReplacementPortal,
+      anchors: bestAnchors
+    };
+    cache_put(ps, ans);
+    return ans;
+  }
+
+  // For each starting triangle, record the best.
+  bestAns = {value: 0, parentPortals: undefined, replacementPortal: undefined, anchors: undefined};
+  bestAnsPortals = undefined;
+  for (var i = 0; i < thePortals.length; i++) {
+    for (var j = i+1; j < thePortals.length; j++) {
+      for (var k = j+1; k < thePortals.length; k++) {
+        // Colinear portals shouldn't be considered.
+        if (side(thePortals[i], thePortals[j], thePortals[k]) == 0) continue;
+
+        var ans = howManyLayers([i, j, k]);
+        if (ans.value > bestAns.value) {
+          bestAns = ans;
+          bestAnsPortals = [thePortals[i], thePortals[j], thePortals[k]];
+        }
+      }
+    }
+  }
+
+  // Retrieve all the layers.
+  var replacementPortals = [];
+  var anchors = [];
+  var curPortals = bestAnsPortals;
+  var curAns = bestAns;
+  while (curAns.parentPortals !== undefined) {
+    // Find the new portal and add it to the list.
+    replacementPortals.push(curAns.replacementPortal);
+    anchors.push(curAns.anchors);
+
+    // Move up the parent tree.
+    curPortals = curAns.parentPortals;
+    curAns = cache_get(curAns.parentPortals);
+  }
+
+  console.log("Best ans ",bestAns.value ," is base", bestAnsPortals, "with additional layers from", replacementPortals);
+
+  return {base: bestAnsPortals, replacements: replacementPortals, anchors: anchors};
+}
+
+
+
+
 
 function test_shortest_path_graph() {
 	var ants, generations, aco, graph, i, adj_list;
@@ -792,6 +930,11 @@ function test_shortest_path_graph() {
 	console.log("bfs", graph.getShortestPath());
 }
 
+
+/*****************************************************************************
+ * Testing                                                                   *
+ *****************************************************************************/
+
 var test_points = [
 	new Point(5, 5),
 	new Point(6, 9),
@@ -803,50 +946,25 @@ var test_points = [
 	new Point(9, 7)
 ];
 
-function test_literal_graph(num_points) {
-	var i;
 
-	/*
-	var ant = new LiteralAnt(graph, function(es){return es[0];});
-
-	ant.step();
-	ant.step();
-	console.log(ant)
-	*/
-
-	var graph = new LiteralGraph(points);
-	
-	var parameters = {
-		initial_pheromone: 0,
-		alpha: 0.8,
-		beta: 0.3,
-		evaporation_decay: 0.3
-	}
-	var aco = new ACO(graph, parameters, LiteralAnt, 100);
-
-	console.info = function() {}
-
-	for (i = 0; i < 100; i++) {
-		aco.runGeneration();
-	}
-
-	console.log(aco.solutions);
-}
 
 function random_points(num_points) {
-	var points = [];
+	var i,
+		points = [];
 	for (i = 0; i < num_points; i++) {
 		points.push(new Point(Math.random() * num_points, Math.random() * num_points));
 	}
 	return points;
 }
 
+var fs = require('fs');
 var program = require('commander');
 program
 	.version('0.1.0')
 	.option('-a, --num-ants <count>', "Number of ants", 10)
 	.option('-g, --num-generations <count>', "Number of generations", 10)
 	.option('-r, --random-points <count>', "Use <count> randomly generated points", 10)
+	.option('-l, --log-file <file>', "Write stats to <file>")
 	.option('-p, --points [file]', "Load portals (points) from the specified file or stdin");
 
 program
@@ -867,8 +985,8 @@ program
 
 
 		points = test_points;
-		if (program.randomPortals) {
-			points = random_points(program.randomPortals);
+		if (program.randomPoints) {
+			points = random_points(program.randomPoints);
 		} if (program.points === true) {
 			//TODO read points from stdin
 			throw new Error("not yet implemented");
@@ -891,15 +1009,25 @@ program
 			break;
 			default:
 				throw new Error("Unknown construction '"+construction+"'");
-		}	
+		}
+
+		console.info = function() {}
 
 		graph = new LiteralGraph(points);
-		aco = new ACOVarient(graph, {}, LiteralAnt, program.numAnts);
+		aco = new ACOVarient(graph, {initial_pheromone: 10000}, LiteralAnt, program.numAnts);
 		console.log(aco);
 		for (i = 0; i < program.numGenerations; i++) {
 			aco.runGeneration();
 		}
-		console.log(aco.solutions)
+		console.log(aco.solutions);
+		if (program.logFile) {
+			var stats = {
+				"generation bests": aco.solutions.generation_goodnesses,
+				"generation pheromone bests":  aco.generation_pheromones,
+			}
+			console.log(stats)
+			fs.writeFileSync(program.logFile, JSON.stringify(stats));
+		}
 	})
 
 program.parse(process.argv)
