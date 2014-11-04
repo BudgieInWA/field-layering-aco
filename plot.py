8db033ce5799436c267c4ee4a991e7800f01b824
BudgieInWA <benjaminaritter@gmail.com>
2014-11-04 11:49:36 +0800

    Added a python script to plot stats

---
+++ plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plot
+import json
+
+def main(script, fn, *args):
+    with open(fn, 'r') as f:
+        stats = json.loads(f.read())
+
+        print (stats)
+
+        for key in stats:
+            plot.figure(key)
+            plot.title(key)
+            plot.plot(range(len(stats[key])), stats[key])
+
+        plot.show()
+
+if __name__ == '__main__':
+    import sys
+    main(*sys.argv)
+            
