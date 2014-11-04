df20ee16e75d42437d971118b26fc7f025a48b80
BudgieInWA <benjaminaritter@gmail.com>
2014-11-04 16:35:18 +0800

    Added saving to disk

--- plot.py
+++ plot.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plot
 import json
 
-def main(script, fn, *args):
+def main(script, fn, prefix='saved', *args):
     with open(fn, 'r') as f:
         stats = json.loads(f.read())
 
         for key in stats:
-            plot.figure(key)
-            plot.title(key)
+            plot.figure(prefix + " " + key)
+            plot.title(prefix + " " + key)
             plot.plot(range(len(stats[key])), stats[key])
+
+            plot.savefig(prefix+"-"+fn+"-"+key+".png")
+
 
         plot.show()
 
